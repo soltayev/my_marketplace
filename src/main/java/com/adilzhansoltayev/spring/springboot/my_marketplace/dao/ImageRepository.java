811dcc10cad831df7daac84835922f7c613923a9
soltayev <adilzhan.soltayev@gmail.com>
2025-02-01 10:45:26 +0500

    etc

--- src/main/java/com/adilzhansoltayev/spring/springboot/my_marketplace/dao/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.adilzhansoltayev.spring.springboot.my_marketplace.dao;
-
-import com.adilzhansoltayev.spring.springboot.my_marketplace.entity.Images;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ImageRepository extends JpaRepository<Images, Integer> {
-}
--- src/main/java/com/adilzhansoltayev/spring/springboot/my_marketplace/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.adilzhansoltayev.spring.springboot.my_marketplace.service;
-
-import com.adilzhansoltayev.spring.springboot.my_marketplace.entity.Images;
-
-public interface ImageService {
-    public Images getImage(int id);
-}
